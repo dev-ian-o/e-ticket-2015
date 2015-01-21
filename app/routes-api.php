b122ad09384ea72091b7239c1bff58096bba270e
ianolinares <dev.magente.cjolinares.gmail.com>
2015-01-22 03:52:09 +0800

    minified blade templating

--- app/routes-api.php
+++ app/routes-api.php
@@ -13,14 +13,20 @@ Route::group(array('prefix' => 'api/v1'), function()
 
 Route::group(array('prefix' => 'admin'), function()
 {
-	 Route::get('/', function(){
-	 	return View::make('admin.index');
-	 });
+	Route::get('/', 			function(){ return View::make('admin.ui-buttons'); });
+	Route::get('/home', 		function(){ return View::make('admin.home'); });
+	Route::get('/students', 	function(){ return View::make('admin.students'); });
+	Route::get('/tickets', 		function(){ return View::make('admin.tickets'); });
+	Route::get('/events', 		function(){ return View::make('admin.events'); });
+	Route::get('/viewport', 	function(){ return View::make('admin.viewport'); });
+	Route::get('/registration', function(){ return View::make('admin.registration'); });
+	Route::get('/accounting', 	function(){ return View::make('admin.accounting'); });
 
-	 Route::get('/', function(){
-	 	return View::make('admin.ui-buttons');
-	 });
+});
 
+Route::group(array('prefix' => 'ui-admin'), function()
+{
+	 Route::get('/', function(){ return View::make('ui-admin.index'); });
+	 Route::get('/', function(){ return View::make('ui-admin.ui-buttons');});
 
-    
 });
---
+++ app/views/ui-admin/common/breadcrumbs.blade.php
@@ -0,0 +1,7 @@
+        <!-- START BREADCRUMB -->
+        <ul class="breadcrumb">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">UI Kits</a></li>
+            <li class="active">Buttons</li>
+        </ul>
+        <!-- END BREADCRUMB -->
